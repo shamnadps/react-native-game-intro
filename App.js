1b2d2dc9a64ec6bd3b9f1757dfa978152a546182
Shamnad Shaji <shamnad.shaji@gofore.com>
2019-01-12 15:28:47 +0200

    Just flowing balls in random

--- App.js
+++ App.js
@@ -15,61 +15,56 @@ import { GameEngine } from "react-native-game-engine";
 
 const { width, height } = Dimensions.get("screen");
 const boxSize = Math.trunc(Math.max(width, height) * 0.075);
-const initialBox1 = Matter.Bodies.rectangle(boxSize, 0, boxSize, boxSize,
-  {
-    frictionAir: 0.07
-  });
 
-const initialBox2 = Matter.Bodies.rectangle(boxSize + 0.9 * width / 4, 0, boxSize, boxSize,
-  {
-    frictionAir: 0.07
-  });
-const initialBox3 = Matter.Bodies.rectangle(boxSize + 1.8 * width / 4, 0, boxSize, boxSize,
-  {
-    frictionAir: 0.07
-  });
-const initialBox4 = Matter.Bodies.rectangle(boxSize + 2.7 * width / 4, 0, boxSize, boxSize,
-  {
-    frictionAir: 0.07
-  });
 const floor = Matter.Bodies.rectangle(width / 2, height - boxSize / 2, width, boxSize, { isStatic: true });
 const top = Matter.Bodies.rectangle(width / 2, 0 + boxSize / 2, width, boxSize, { isStatic: true });
 
 const engine = Matter.Engine.create({ enableSleeping: false });
 const world = engine.world;
-Matter.World.add(world, [initialBox1]);
-Matter.World.add(world, [initialBox2]);
-Matter.World.add(world, [initialBox3]);
-Matter.World.add(world, [initialBox4]);
 
 let boxIds = 0;
 const CreateBox = (entities, { touches, screen }) => {
   let world = entities["physics"].world;
   let boxSize = Math.trunc(Math.max(screen.width, screen.height) * 0.075);
-
+  let body = Matter.Bodies.rectangle(
+    boxSize, 0,
+    boxSize, boxSize);
+  Matter.World.add(world, [body]);
   entities[++boxIds] = {
-    body: initialBox1,
+    body: body,
     size: [boxSize, boxSize],
     color: boxIds % 2 == 0 ? "pink" : "#B8E986",
     renderer: Box
   };
 
+  let body2 = Matter.Bodies.rectangle(
+    boxSize + 0.9 * width / 4, 0,
+    boxSize, boxSize);
+  Matter.World.add(world, [body2]);
   entities[++boxIds] = {
-    body: initialBox2,
+    body: body2,
     size: [boxSize, boxSize],
     color: boxIds % 2 == 0 ? "pink" : "#B8E986",
     renderer: Box
   };
 
+  let body3 = Matter.Bodies.rectangle(
+    boxSize + 1.8 * width / 4, 0,
+    boxSize, boxSize);
+  Matter.World.add(world, [body3]);
   entities[++boxIds] = {
-    body: initialBox3,
+    body: body3,
     size: [boxSize, boxSize],
     color: boxIds % 2 == 0 ? "pink" : "#B8E986",
     renderer: Box
   };
 
+  let body4 = Matter.Bodies.rectangle(
+    boxSize + 2.7 * width / 4, 0,
+    boxSize, boxSize);
+  Matter.World.add(world, [body4]);
   entities[++boxIds] = {
-    body: initialBox4,
+    body: body4,
     size: [boxSize, boxSize],
     color: boxIds % 2 == 0 ? "pink" : "#B8E986",
     renderer: Box
