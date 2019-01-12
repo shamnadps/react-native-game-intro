6045f2ef306ef58b76fea954510c4fe57725d918
Shamnad Shaji <shamnad.shaji@gofore.com>
2019-01-12 15:09:49 +0200

    add dropping animation to balls

--- App.js
+++ App.js
@@ -15,35 +15,66 @@ import { GameEngine } from "react-native-game-engine";
 
 const { width, height } = Dimensions.get("screen");
 const boxSize = Math.trunc(Math.max(width, height) * 0.075);
-const initialBox = Matter.Bodies.rectangle(width / 2, height / 2, boxSize, boxSize);
+const initialBox1 = Matter.Bodies.rectangle(boxSize, 0, boxSize, boxSize,
+  {
+    frictionAir: 0.07
+  });
+
+const initialBox2 = Matter.Bodies.rectangle(boxSize + 0.9 * width / 4, 0, boxSize, boxSize,
+  {
+    frictionAir: 0.07
+  });
+const initialBox3 = Matter.Bodies.rectangle(boxSize + 1.8 * width / 4, 0, boxSize, boxSize,
+  {
+    frictionAir: 0.07
+  });
+const initialBox4 = Matter.Bodies.rectangle(boxSize + 2.7 * width / 4, 0, boxSize, boxSize,
+  {
+    frictionAir: 0.07
+  });
 const floor = Matter.Bodies.rectangle(width / 2, height - boxSize / 2, width, boxSize, { isStatic: true });
 const top = Matter.Bodies.rectangle(width / 2, 0 + boxSize / 2, width, boxSize, { isStatic: true });
 
 const engine = Matter.Engine.create({ enableSleeping: false });
 const world = engine.world;
-Matter.World.add(world, [initialBox, floor]);
-
+Matter.World.add(world, [initialBox1]);
+Matter.World.add(world, [initialBox2]);
+Matter.World.add(world, [initialBox3]);
+Matter.World.add(world, [initialBox4]);
 
 let boxIds = 0;
 const CreateBox = (entities, { touches, screen }) => {
   let world = entities["physics"].world;
   let boxSize = Math.trunc(Math.max(screen.width, screen.height) * 0.075);
-  touches.filter(t => t.type === "press").forEach(t => {
-    let body = Matter.Bodies.rectangle(
-      t.event.pageX, t.event.pageY,
-      boxSize, boxSize,
-      {
-        frictionAir: 0.021,
-        restitution: 1.0
-      });
-    Matter.World.add(world, [body]);
-    entities[++boxIds] = {
-      body: body,
-      size: [boxSize, boxSize],
-      color: boxIds % 2 == 0 ? "pink" : "#B8E986",
-      renderer: Box
-    };
-  });
+
+  entities[++boxIds] = {
+    body: initialBox1,
+    size: [boxSize, boxSize],
+    color: boxIds % 2 == 0 ? "pink" : "#B8E986",
+    renderer: Box
+  };
+
+  entities[++boxIds] = {
+    body: initialBox2,
+    size: [boxSize, boxSize],
+    color: boxIds % 2 == 0 ? "pink" : "#B8E986",
+    renderer: Box
+  };
+
+  entities[++boxIds] = {
+    body: initialBox3,
+    size: [boxSize, boxSize],
+    color: boxIds % 2 == 0 ? "pink" : "#B8E986",
+    renderer: Box
+  };
+
+  entities[++boxIds] = {
+    body: initialBox4,
+    size: [boxSize, boxSize],
+    color: boxIds % 2 == 0 ? "pink" : "#B8E986",
+    renderer: Box
+  };
+
   return entities;
 };
 
@@ -62,11 +93,6 @@ export default class App extends Component {
           physics: {
             engine: engine,
             world: world
-          },
-          initialBox: {
-            body: initialBox,
-            size: [boxSize, boxSize],
-            renderer: Box,
           },
           floor: {
             body: floor,
